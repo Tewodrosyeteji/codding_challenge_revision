4e8ba3fdd34fa8fabef35c64985b687f2a929b0f
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-03-18 12:10:19 +0300

    recursion 2

---
+++ recursion2.js
@@ -0,0 +1,23 @@
+// Let’s define a peculiar type of array in which each element is either an integer or another peculiar array. 
+// Assume that a peculiar array is never empty.
+//  Write a function that will take a peculiar array as its input and 
+//  find the sum of its elements. If an array is an element in 
+//  the peculiar array you have to convert it to it’s equivalent value so that you can sum it with the other elements. 
+//  Equivalent value of an array is the sum of its elements raised to the number which represents how far nested it is. 
+//  For e.g. [2,3,[4,1,2]] = 2+3+ (4+1+2)^2 
+// [1,2,[7,[3,4],2]] = 1 + 2 +( 7+(3+4)^3+2)^2
+
+const powerSum=function(array,power=1){
+    let sum=0;
+    for(const element of array){
+        if(Array.isArray(element)){
+            sum += powerSum(element,power+1)
+        }else{
+            sum +=element;
+        }
+    }
+    return Math.pow(sum,power)
+
+}
+
+console.log(powerSum([1,2,3,[4]]))
